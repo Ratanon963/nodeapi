c51891531779b85bea5c1213dbcf303f4e8b3b1e
Ratanon <Ratanon.so@gmail.com>
2020-01-03 15:59:37 +1100

    completed delete user function

--- nodeapi/controllers/user.js
+++ nodeapi/controllers/user.js
@@ -1,4 +1,5 @@
 const User = require('../models/user');
+const _ = require('lodash');
 
 
 exports.userById = (req,res,next,id) => {
@@ -40,3 +41,43 @@ exports.allUsers = (req, res) => {
         res.json(users);
     }).select('name email updated create ');
 };
+
+
+exports.getUser = (req, res) => {
+
+    req.profile.hashed_password = undefined;
+    req.profile.salt = undefined;
+    return res.json(req.profile);
+}
+
+
+ exports.updateUser = (req, res, next) =>{
+    let user = req.profile;
+    user = _.extend(user, req.body); // extend - muate the source object
+    user.update = Date.now();
+    user.save((err) => {
+        if(err){
+            return res.status(400).json({
+                error: "You are not authorized to perform this action"
+            });
+        }
+        user.hashed_password = undefined;
+        user.salt = undefined;
+        res.json({user});
+    });
+
+ }
+
+exports.deleteUser = (req,res,next) => {
+    let user = req.profile;
+    user.remove((err, user) => {
+        if(err){
+            return res.status(400).json({
+                error:err
+            });
+        }
+        res.json({ message: "User deleted successfully"});
+    });
+
+}
+
